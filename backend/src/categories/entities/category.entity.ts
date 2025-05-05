4b729b38f419b611f376ce9f4de9c37b4df2664d
JuanPablo Valdebenito <jpablo.valdebenitom@gmail.com>
2025-05-05 16:52:51 -0400

    Relacion uno a muchos con Product

--- backend/src/categories/entities/category.entity.ts
+++ backend/src/categories/entities/category.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Product } from "../../products/entities/product.entity";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class Category {
@@ -7,4 +8,7 @@ export class Category {
 
   @Column({ type: 'varchar', length: 60})
   name: string
+
+  @OneToMany(() => Product, (product) => product.category, {cascade: true} )
+  products: Product[]
 }

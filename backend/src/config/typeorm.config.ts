0532c170b5948f67dd2e59bf90ccd61f25bd0177
Juan Pablo <jpablo.valdebenitom@gmail.com>
2025-05-04 22:52:04 -0400

    + config typeOrm

--- backend/src/config/typeorm.config.ts
+++ backend/src/config/typeorm.config.ts
@@ -3,5 +3,11 @@ import type { TypeOrmModuleOptions } from "@nestjs/typeorm"
 
 export const typeOrmConfig = (configService : ConfigService) : TypeOrmModuleOptions => ({
   type: 'postgres',
-  host: configService.get('DATABASE_HOST')
+  host: configService.get('DATABASE_HOST'),
+  port: configService.get('DATABASE_PORT'),
+  username: configService.get('DATABASE_USER'),
+  password: configService.get('DATABASE_PASSWORD'),
+  database: configService.get('DATABASE_NAME'),
+  ssl: false,
+  logging: true,
 })

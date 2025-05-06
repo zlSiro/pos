9caf4645db9b5afc6768defe6dbabd4ba751b6b3
JuanPablo Valdebenito <jpablo.valdebenitom@gmail.com>
2025-05-06 18:54:25 -0400

    + update

--- backend/src/products/dto/create-product.dto.ts
+++ backend/src/products/dto/create-product.dto.ts
@@ -1,1 +1,19 @@
-export class CreateProductDto {}
+import { IsInt, IsNotEmpty, IsNumber, IsString } from "class-validator"
+
+export class CreateProductDto {
+  @IsNotEmpty({message: "El nombre del producto es obligatorio"})
+  @IsString({message: "Nombre no válido"})
+  name: string
+
+  @IsNotEmpty({message: "El precio del producto es obligatorio"})
+  @IsNumber({maxDecimalPlaces: 2}, {message: "Precio no válido"})
+  price: number
+
+  @IsNotEmpty({message: "La cantidad no puede ir vacia"})
+  @IsNumber({maxDecimalPlaces: 0}, {message: "cantidad no válida"})
+  inventory: number
+
+  @IsNotEmpty({message: "La categoria es obligatoria"})
+  @IsInt({message: "La categoria no es válida"})
+  categoryId: number
+}
--- backend/src/products/entities/product.entity.ts
+++ backend/src/products/entities/product.entity.ts
@@ -1,1 +1,23 @@
-export class Product {}
+import { Category } from "../../categories/entities/category.entity";
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column({ type: "varchar", length: 60 })
+  name: string
+
+  @Column({type: "varchar", length: 120, nullable: true, default: "default.svg"})
+  image: string
+
+  @Column({type: "decimal"})
+  price: number
+
+  @Column({type: "int"})
+  inventory: number
+
+  @ManyToOne(() => Category)
+  category: Category
+}

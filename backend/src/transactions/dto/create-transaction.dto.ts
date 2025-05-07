824a5edf1c36b61e4b9c02c822262a8420950bbf
JuanPablo Valdebenito <jpablo.valdebenitom@gmail.com>
2025-05-07 11:58:15 -0400

    + logica agregada al dto

--- backend/src/transactions/dto/create-transaction.dto.ts
+++ backend/src/transactions/dto/create-transaction.dto.ts
@@ -1,1 +1,29 @@
-export class CreateTransactionDto {}
+import { Type } from "class-transformer";
+import {  ArrayNotEmpty, IsArray, IsInt, IsNotEmpty, IsNumber, IsOptional, Length, ValidateNested } from "class-validator";
+
+export class TransactionContentsDto {
+  @IsNotEmpty({ message: 'El ID del producto no puede estar vacío' })
+  @IsInt({ message: 'Producto no válido' })
+  productId: number;
+
+  @IsNotEmpty({ message: 'Cantidad no puede estar vacía' })
+  @IsInt({ message: 'Cantidad no válida' }) // Validate quantity too
+  quantity: number;
+
+  @IsNotEmpty({ message: 'Precio no puede estar vacío' })
+  @IsNumber({}, { message: 'Precio no válido' })
+  price: number;
+}
+
+export class CreateTransactionDto {
+  @IsNotEmpty({message: 'El Total no puede ir vacio'})
+  @IsNumber({}, {message: 'Cantidad no válida'})
+  total: number
+
+  @IsArray()
+  @ArrayNotEmpty({message: 'Los Contenidos no pueden ir vacios'})
+  @ValidateNested()
+  @Type(() => TransactionContentsDto)
+  contents: TransactionContentsDto[]
+}
+

67677031f8ba095db9c5168daef47463485b5c91
JuanPablo Valdebenito <jpablo.valdebenitom@gmail.com>
2025-05-07 10:55:53 -0400

    + al proyecto

---
+++ backend/src/transactions/entities/transaction.entity.ts
@@ -0,0 +1,35 @@
+import { Product } from "src/products/entities/product.entity";
+import { Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Transaction {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column('decimal')
+  total: number
+
+  @Column({type: "timestamp", default: () =>"CURRENT_TIMESTAMP(6)"})
+  transactionDate: Date
+
+  @OneToMany(() => TransactionContent, (transaction) => transaction.transaction)
+  contents: TransactionContent[]
+}
+
+@Entity()
+export class TransactionContent {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column("int")
+  quatity: number
+
+  @Column("decimal")
+  price: number
+
+  @ManyToOne(() => Product, (product) => product.id, {eager: true})
+  product: Product
+
+  @ManyToOne(() => Transaction, (transaction) => transaction.contents)
+  transaction: Transaction
+}

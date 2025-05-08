ba4ff5c76aefc5ce704d7085500396ef8aaf1cda
JuanPablo Valdebenito <jpablo.valdebenitom@gmail.com>
2025-05-08 19:36:15 -0400

    + logica para el service de coupon

--- backend/src/coupons/coupons.service.ts
+++ backend/src/coupons/coupons.service.ts
@@ -1,26 +1,58 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, UnprocessableEntityException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { endOfDay, isAfter } from 'date-fns';
+import { Repository } from 'typeorm';
 import { CreateCouponDto } from './dto/create-coupon.dto';
 import { UpdateCouponDto } from './dto/update-coupon.dto';
+import { Coupon } from './entities/coupon.entity';
 
 @Injectable()
 export class CouponsService {
+
+  constructor(
+    @InjectRepository(Coupon) private readonly couponRepository : Repository<Coupon>
+  ) {}
   create(createCouponDto: CreateCouponDto) {
-    return 'This action adds a new coupon';
+    return this.couponRepository.save(createCouponDto)
   }
 
   findAll() {
-    return `This action returns all coupons`;
+    return this.couponRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} coupon`;
+  async findOne(id: number) {
+    const coupon = await this.couponRepository.findOneBy({id})
+    if (!coupon) {
+      throw new NotFoundException(`El cupón con el ID: ${id} no existe`)
+    }
+    return coupon;
   }
 
-  update(id: number, updateCouponDto: UpdateCouponDto) {
-    return `This action updates a #${id} coupon`;
+  async update(id: number, updateCouponDto: UpdateCouponDto) {
+    const coupon = await this.findOne(id)
+    Object.assign(coupon, updateCouponDto)
+    return await this.couponRepository.save(coupon)
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} coupon`;
+  async remove(id: number) {
+    const coupon = await this.findOne(id)
+    await this.couponRepository.remove(coupon)
+    return {message: `Cupon eliminado exitosamente`};
+  }
+
+  async applyCoupon(name: string) {
+    const coupon = await this.couponRepository.findOneBy({name})
+    if (!coupon) {
+      throw new NotFoundException("El cupón no existe...")
+    }
+    const currentDate = new Date()
+    const expirationDate = endOfDay(coupon.expirationDate)
+    if(isAfter(currentDate, expirationDate)) {
+      throw new UnprocessableEntityException("Cupón ya expirado")
+    }
+    return {
+      message: "Cupon válido",
+      ...coupon
+    }
   }
 }

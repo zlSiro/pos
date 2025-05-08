612810b00bf7c74c792b9ca1b5b5927e385d7c4e
JuanPablo Valdebenito <jpablo.valdebenitom@gmail.com>
2025-05-08 19:34:54 -0400

    exportado el modulo de coupon

--- backend/src/coupons/coupons.module.ts
+++ backend/src/coupons/coupons.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { CouponsService } from './coupons.service';
 import { CouponsController } from './coupons.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Coupon } from './entities/coupon.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Coupon])],
   controllers: [CouponsController],
   providers: [CouponsService],
+  exports: [CouponsService]
 })
 export class CouponsModule {}

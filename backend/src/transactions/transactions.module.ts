cd9fddfe142353faaab0fff11f89cda2e33b08e3
JuanPablo Valdebenito <jpablo.valdebenitom@gmail.com>
2025-05-08 19:34:42 -0400

    importado el modulo de coupon para usar el service de coupon

--- backend/src/transactions/transactions.module.ts
+++ backend/src/transactions/transactions.module.ts
@@ -4,9 +4,17 @@ import { TransactionsController } from './transactions.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Transaction, TransactionContent } from './entities/transaction.entity';
 import { Product } from 'src/products/entities/product.entity';
+import { CouponsModule } from 'src/coupons/coupons.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Transaction, TransactionContent, Product])],
+  imports: [
+    TypeOrmModule.forFeature([
+      Transaction,
+      TransactionContent,
+      Product
+    ]),
+    CouponsModule
+  ],
   controllers: [TransactionsController],
   providers: [TransactionsService],
 })

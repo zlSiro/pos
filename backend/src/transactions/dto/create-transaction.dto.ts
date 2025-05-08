20db00756870e3b913d1fd82cc7ba59d7369de8d
JuanPablo Valdebenito <jpablo.valdebenitom@gmail.com>
2025-05-08 19:33:56 -0400

    update dto de transaction

--- backend/src/transactions/dto/create-transaction.dto.ts
+++ backend/src/transactions/dto/create-transaction.dto.ts
@@ -20,6 +20,9 @@ export class CreateTransactionDto {
   @IsNumber({}, {message: 'Cantidad no válida'})
   total: number
 
+  @IsOptional()
+  coupon: string
+
   @IsArray()
   @ArrayNotEmpty({message: 'Los Contenidos no pueden ir vacios'})
   @ValidateNested()

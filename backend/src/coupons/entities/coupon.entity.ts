e95161966cf4564a8bba02b59a9f43c563f0199b
JuanPablo Valdebenito <jpablo.valdebenitom@gmail.com>
2025-05-08 13:55:08 -0400

    Creada la entity de Coupon

--- backend/src/coupons/entities/coupon.entity.ts
+++ backend/src/coupons/entities/coupon.entity.ts
@@ -1,1 +1,16 @@
-export class Coupon {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Coupon {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column({type: "varchar", length: 30})
+  name: string
+
+  @Column({type: "int"})
+  percentage: number
+
+  @Column({type: "date"})
+  expirationDate: Date
+}

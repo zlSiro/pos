f4485d8c37efde19ef7ccea5e2dbcba1414b48f1
JuanPablo Valdebenito <jpablo.valdebenitom@gmail.com>
2025-05-08 11:40:44 -0400

    añadido Product a los imports

--- backend/src/transactions/transactions.module.ts
+++ backend/src/transactions/transactions.module.ts
@@ -3,9 +3,10 @@ import { TransactionsService } from './transactions.service';
 import { TransactionsController } from './transactions.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Transaction, TransactionContent } from './entities/transaction.entity';
+import { Product } from 'src/products/entities/product.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Transaction, TransactionContent])],
+  imports: [TypeOrmModule.forFeature([Transaction, TransactionContent, Product])],
   controllers: [TransactionsController],
   providers: [TransactionsService],
 })

c2ba1946b3a7f6f6de3d0606fe82e36f95ab796d
JuanPablo Valdebenito <jpablo.valdebenitom@gmail.com>
2025-05-08 12:21:15 -0400

    importado el modulo de coupons

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,6 +7,7 @@ import { CategoriesModule } from './categories/categories.module';
 import { typeOrmConfig } from './config/typeorm.config';
 import { ProductsModule } from './products/products.module';
 import { TransactionsModule } from './transactions/transactions.module';
+import { CouponsModule } from './coupons/coupons.module';
 
 @Module({
   imports: [
@@ -19,7 +20,8 @@ import { TransactionsModule } from './transactions/transactions.module';
     }),
     CategoriesModule,
     ProductsModule,
-    TransactionsModule
+    TransactionsModule,
+    CouponsModule
   ],
   controllers: [AppController],
   providers: [AppService],

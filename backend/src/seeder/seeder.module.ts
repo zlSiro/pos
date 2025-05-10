9645f24e56a08293cfb3e95f0a130bee893b68d7
JuanPablo Valdebenito <jpablo.valdebenitom@gmail.com>
2025-05-10 00:38:34 -0400

    Se agregan los imports para el seeder

--- backend/src/seeder/seeder.module.ts
+++ backend/src/seeder/seeder.module.ts
@@ -3,6 +3,8 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from '../config/typeorm.config';
 import { SeederService } from './seeder.service';
+import { Product } from '../products/entities/product.entity';
+import { Category } from '../categories/entities/category.entity';
 
 @Module({
   imports: [
@@ -13,6 +15,7 @@ import { SeederService } from './seeder.service';
         useFactory: typeOrmConfig,
         inject: [ConfigService]
       }),
+      TypeOrmModule.forFeature([Product, Category])
     ],
   providers: [SeederService]
 })

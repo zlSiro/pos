5f23d644c41260d055e28d22d84a669901835205
JuanPablo Valdebenito <jpablo.valdebenitom@gmail.com>
2025-05-10 00:43:50 -0400

    Se crea funcionalidad para usar archivos staticos de en la app

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true
   }))
+  app.useStaticAssets(join(__dirname, "../public"))
   const port = process.env.PORT ?? 3000;
   await app.listen(port);
   console.log(`Servidor escuchando en el puerto: ${port}`)

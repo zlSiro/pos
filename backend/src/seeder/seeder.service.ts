e7d7d059bf470b76e9d0c1d856173acf2dfc6e19
JuanPablo Valdebenito <jpablo.valdebenitom@gmail.com>
2025-05-10 00:38:57 -0400

    Creada la logica para manejar la subida de archivos con el seeder

--- backend/src/seeder/seeder.service.ts
+++ backend/src/seeder/seeder.service.ts
@@ -1,8 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository, DataSource } from 'typeorm';
+import { Category } from '../categories/entities/category.entity';
+import { Product } from '../products/entities/product.entity';
+import { categories } from './data/categories';
+import { products } from './data/products';
 
 @Injectable()
 export class SeederService {
+
+  constructor(
+    @InjectRepository(Category) private readonly categoryRepository : Repository<Category>,
+    @InjectRepository(Product) private readonly productRepository : Repository<Product>,
+    private readonly dataSource : DataSource
+  ) {}
+
+  async onModuleInit() {
+    const connection = this.dataSource
+    await connection.dropDatabase()
+    await connection.synchronize()
+  }
+
   async seed() {
-    console.log("desde seed en seeder.service.ts")
+    await this.categoryRepository.save(categories);
+    for await (const seedProduct of products) {
+      const category = await this.categoryRepository.findOneBy({id: seedProduct.categoryId})
+      if (!category) {
+        throw new NotFoundException("La categoria no existe...")
+      }
+      
+      const product = new Product()
+      product.name = seedProduct.name
+      product.image = seedProduct.image
+      product.price = seedProduct.price
+      product.inventory = seedProduct.inventory
+      product.category = category
+
+      await this.productRepository.save(product)
+    }
   }
 }

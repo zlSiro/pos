6726c5c9418943e08b727f213588f4efc3615e4d
JuanPablo Valdebenito <jpablo.valdebenitom@gmail.com>
2025-05-10 00:37:11 -0400

    creadas las entidades para el seeder

---
+++ backend/src/seeder/data/categories.ts
@@ -0,0 +1,11 @@
+export const categories = [
+  {
+    "name": "Sudaderas"
+  },
+  {    
+    "name": "Tenis"
+  },
+  {    
+    "name": "Lentes"
+  }
+]
---
+++ backend/src/seeder/data/products.ts
@@ -0,0 +1,282 @@
+export const products = [
+  {
+    "name": "Sudadera Gris",
+    "image": "1.jpg",
+    "price": 49,
+    "inventory": 5,
+    "categoryId": 1
+  },
+  {    
+    "name": "Sudadera Naranja Hombre",
+    "image": "2.jpg",
+    "price": 29,
+    "inventory": 3,
+    "categoryId": 1
+  },
+  {    
+    "name": "Sudadera Azul",
+    "image": "3.jpg",
+    "price": 49,
+    "inventory": 8,
+    "categoryId": 1
+  },
+  {    
+    "name": "Sudadera Blanca",
+    "image": "4.jpg",
+    "price": 29,
+    "inventory": 10,
+    "categoryId": 1
+  },
+  {    
+    "name": "Sudadera Negra / Naranja",
+    "image": "5.jpg",
+    "price": 49,
+    "inventory": 10,
+    "categoryId": 1
+  },
+  {    
+    "name": "Sudadera Negra / Rojo",
+    "image": "6.jpg",
+    "price": 39,
+    "inventory": 10,   
+    "categoryId": 1
+  },
+  {    
+    "name": "Sudadera Rojo",
+    "image": "7.jpg",
+    "price": 49,
+    "inventory": 10,
+    "categoryId": 1
+  },
+  {    
+    "name": "Sudadera Blanco / Azul",
+    "image": "8.jpg",
+    "price": 49,
+    "inventory": 10,
+    "categoryId": 1
+  },
+  {    
+    "name": "Sudadera Amarillo Mujer",
+    "image": "9.jpg",
+    "price": 39,
+    "inventory": 10,
+    "categoryId": 1
+  },
+  {
+    "name": "Tenis Mujer",
+    "image": "10.jpg",
+    "price": 59,
+    "inventory": 10,   
+    "categoryId": 2
+  },
+  {
+    "name": "Zapato Casual",
+    "image": "11.jpg",
+    "price": 49,
+    "inventory": 10,   
+    "categoryId": 2
+  },
+  {
+    "name": "Tenis Mujer Rosa",
+    "image": "12.jpg",
+    "price": 49,
+    "inventory": 10,   
+    "categoryId": 2
+  },
+  {
+    "name": "Tenis Gris",
+    "image": "13.jpg",
+    "price": 79,
+    "inventory": 10,   
+    "categoryId": 2
+  },
+  {
+    "name": "Tenis Rojo / Negro",
+    "image": "14.jpg",
+    "price": 69,
+    "inventory": 10,   
+    "categoryId": 2
+  },
+  {
+    "name": "Tenis Mujer Deporte",
+    "image": "15.jpg",
+    "price": 69,
+    "inventory": 10,   
+    "categoryId": 2
+  },
+  {
+    "name": "Tenis Azul Hombre",
+    "image": "16.jpg",
+    "price": 69,
+    "inventory": 10,   
+    "categoryId": 2
+  },
+  {
+    "name": "Tenis Azul",
+    "image": "17.jpg",
+    "price": 69,
+    "inventory": 10,   
+    "categoryId": 2
+  },
+  {
+    "name": "Tenis Casual Hombre",
+    "image": "18.jpg",
+    "price": 59,
+    "inventory": 10,   
+    "categoryId": 2
+  },
+  {
+    "name": "Tenis Casual Gris",
+    "image": "19.jpg",
+    "price": 49,
+    "inventory": 10,   
+    "categoryId": 2
+  },
+  {
+    "name": "Tenis Bota Claros",
+    "image": "20.jpg",
+    "price": 59,
+    "inventory": 10,   
+    "categoryId": 2
+  },
+  {
+    "name": "Tenis Bota Negro",
+    "image": "21.jpg",
+    "price": 59,
+    "inventory": 10,   
+    "categoryId": 2
+  },
+  {
+    "name": "Tenis Bota Negro / Azul",
+    "image": "22.jpg",
+    "price": 59,
+    "inventory": 10,   
+    "categoryId": 2
+  },
+  {
+    "name": "Tenis Casual Negro",
+    "image": "23.jpg",
+    "price": 49,
+    "inventory": 10,   
+    "categoryId": 2
+  },
+  {
+    "name": "Tenis Casual Gris / Negro",
+    "image": "24.jpg",
+    "price": 49,
+    "inventory": 10,   
+    "categoryId": 2
+  },
+  {
+    "name": "Tenis Hombre Negro",
+    "image": "25.jpg",
+    "price": 59,
+    "inventory": 10,   
+    "categoryId": 2
+  },
+  {
+    "name": "Tenis Hombre Gris",
+    "image": "26.jpg",
+    "price": 69,
+    "inventory": 10,   
+    "categoryId": 2
+  },
+  {
+    "name": "Zapato Gris Hombre",
+    "image": "27.jpg",
+    "price": 69,
+    "inventory": 10,   
+    "categoryId": 2
+  },
+  {
+    "name": "Tenis Gris / Rojo",
+    "image": "28.jpg",
+    "price": 49,
+    "inventory": 10,
+    "categoryId": 2
+  },
+  {
+    "name": "Tenis Negro Mujer",
+    "image": "29.jpg",
+    "price": 59,
+    "inventory": 10,   
+    "categoryId": 2
+  },
+  {
+    "name": "Tenis Azul Casual",
+    "image": "30.jpg",
+    "price": 69,
+    "inventory": 10,   
+    "categoryId": 2
+  },
+  {
+    "name": "Tenis Deporte Gris",
+    "image": "31.jpg",
+    "price": 69,
+    "inventory": 10,   
+    "categoryId": 2
+  },
+  {
+    "name": "Tenis Verde Casual",
+    "image": "32.jpg",
+    "price": 59,
+    "inventory": 10,   
+    "categoryId": 2
+  },
+  {
+    "name": "Tenis Rojo Casual",
+    "image": "33.jpg",
+    "price": 59,
+    "inventory": 10,   
+    "categoryId": 2
+  },
+  {
+    "name": "Lentes Sol - Gris",
+    "image": "34.jpg",
+    "price": 39,
+    "inventory": 10,   
+    "categoryId": 3
+  },
+  {
+    "name": "Lentes Sol - Negro",
+    "image": "35.jpg",
+    "price": 39,
+    "inventory": 10,   
+    "categoryId": 3
+  },
+  {
+    "name": "Lentes Oscuros",
+    "image": "36.jpg",
+    "price": 29,
+    "inventory": 10,
+    "categoryId": 3
+  },
+  {
+    "name": "Lentes para Sol",
+    "image": "37.jpg",
+    "price": 49,
+    "inventory": 10,
+    "categoryId": 3
+  },
+  {
+    "name": "Lentes Sol acabado azul",
+    "image": "38.jpg",
+    "price": 39,
+    "inventory": 10,
+    "categoryId": 3
+  },
+  {
+    "name": "Lentes Modernos Negros",
+    "image": "39.jpg",
+    "price": 39,
+    "inventory": 10,
+    "categoryId": 3
+  },
+  {
+    "name": "Lentes Clasicos",
+    "image": "40.jpg",
+    "price": 39,
+    "inventory": 10,
+    "categoryId": 3
+  }
+];

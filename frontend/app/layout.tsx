e19b77812bd1ed9c2d143b4c46600e1d52064af0
JuanPablo Valdebenito <jpablo.valdebenitom@gmail.com>
2025-05-12 10:32:32 -0400

    Se crea al frontend al proyecto

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Outfit } from "next/font/google"
+import "./globals.css";
+
+const outfit = Outfit({subsets: ["latin"]})
+
+export const metadata: Metadata = {
+  title: "POS - Next.js",
+  description: "POS - Next.js",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="es">
+      <body
+        className={`${outfit.className} bg-gray-200`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}

aec15fe71ac091a4b4aabd9615a1aee7dd7afbdf
JuanPablo Valdebenito <jpablo.valdebenitom@gmail.com>
2025-05-13 11:25:13 -0400

    Schemas de zod para el tipado

---
+++ frontend/app/src/schemas.ts
@@ -0,0 +1,21 @@
+import { z } from "zod";
+
+export const ProductSchema = z.object({
+    id: z.number(),
+    name: z.string(),
+    image: z.string(),
+    price: z.coerce.number(),
+    inventory: z.number(),
+    categoryId: z.number()
+})
+
+export const CategorySchema = z.object({
+    id: z.number(),
+    name: z.string()
+})
+
+export const CategoryWithProductsResponseSchema = CategorySchema.extend({
+    products: z.array(ProductSchema)
+});
+
+export type Product = z.infer<typeof ProductSchema>

05bfc6fd04a5ba1f95611cf5abf1f6e78d446cc3
JuanPablo Valdebenito <jpablo.valdebenitom@gmail.com>
2025-05-14 18:50:59 -0400

    + Schemas de zod

--- frontend/src/schemas.ts
+++ frontend/src/schemas.ts
@@ -20,4 +20,19 @@ export const CategoryWithProductsResponseSchema = CategorySchema.extend({
     products: z.array(ProductSchema)
 });
 
+/** Shopping Cart */
+const ShoppingCartContentsSchema = ProductSchema.pick({
+    name: true,
+    image: true,
+    price: true,
+    inventory: true
+}).extend({
+    productId: z.number(),
+    quantity: z.number()
+})
+
+export const ShoppingCartSchema = z.array(ShoppingCartContentsSchema)
+
 export type Product = z.infer<typeof ProductSchema>
+export type ShoppingCart = z.infer<typeof ShoppingCartSchema>
+export type CartItem = z.infer<typeof ShoppingCartContentsSchema>

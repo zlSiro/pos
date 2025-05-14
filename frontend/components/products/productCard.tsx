c7d0bb0b21a004e785d590b206fe5ab89909d3d0
JuanPablo Valdebenito <jpablo.valdebenitom@gmail.com>
2025-05-14 18:50:39 -0400

    se mueve el boton al componente de AddProduct

--- frontend/components/products/productCard.tsx
+++ frontend/components/products/productCard.tsx
@@ -1,6 +1,7 @@
 import { Product } from "@/src/schemas";
 import { formatCurrency } from "@/src/utils";
 import Image from "next/image";
+import AddProductButton from "./AddProductButton";
 
 export default function ProductCard({product} : {product : Product}) {
   return (
@@ -13,21 +14,17 @@ export default function ProductCard({product} : {product : Product}) {
           alt={`Imagen de Producto ${product.name}`}
           width={400}
           height={600}
+          priority
         />
         <div className="p-3 space-y-2">
             <h3 className="text-xl font-bold text-gray-600">{product.name}</h3>
             <p className="text-gray-500">Disponibles: {product.inventory}</p>
             <p className="text-2xl font-extrabold  text-gray-900">{formatCurrency(product.price)}</p>
         </div>
-      </div>
-      <button
-          type="button"
-          className="absolute top-5 -right-3"
-      >
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-8 h-8 bg-indigo-600 rounded-full text-white">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-        </svg>
-      </button>
+      </div> 
+      <AddProductButton
+        product={product}
+      />     
     </div>
   )
 }

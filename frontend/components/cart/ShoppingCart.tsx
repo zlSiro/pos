b7f1a1676e3692bcdc3b42c787ffc2fde098ac12
JuanPablo Valdebenito <jpablo.valdebenitom@gmail.com>
2025-05-14 18:49:17 -0400

    funcionalidad de ShoppingCart

--- frontend/components/cart/ShoppingCart.tsx
+++ frontend/components/cart/ShoppingCart.tsx
@@ -1,6 +1,36 @@
+"use client"
+
+import { useStore } from "@/src/store"
+import ShoppingCartItem from "./ShoppingCartItem"
+import Amount from "./Amount"
 
 export default function ShoppingCart() {
+
+  const contents = useStore(state => state.contents)
+  const total = useStore(state => state.total)
   return (
-    <div>ShoppingCart</div>
+    <>
+      {contents.length ? (
+        <>
+          <h2 className="text-4xl font-bold text-gray-900">Resumen de Venta</h2>
+          <ul role="list" className="mt-6 divide-y divide-gray-200 border-t border-gray-200 text-sm font-medium text-gray-500">
+            {contents.map(item => (
+              <ShoppingCartItem
+                key={item.productId}
+                item={item}
+              />
+            ))}
+          </ul>
+          <dl className="space-y-6 border-t border-gray-300 py-6 text-sm font-medium text-gray-500">
+            <Amount
+              label="Total a Pagar: "
+              amount={total}
+            />
+          </dl>
+        </>
+      ) : (
+        <p className="text-xl text-center text-gray-900">El carrito esta vacio</p>
+      )}
+    </>
   )
 }

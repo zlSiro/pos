b4cd189fcc0950e8b762a6ebae17a8433f4e1215
JuanPablo Valdebenito <jpablo.valdebenitom@gmail.com>
2025-05-20 13:30:09 -0400

    Api interna de next para hacer consultas

--- frontend/app/coupons/api/route.tsx
+++ frontend/app/coupons/api/route.tsx
@@ -1,5 +1,15 @@
 
 
 export async function POST(request: Request) {
-  console.log(await request.json())
+  const coupon = await request.json()
+  const url = `${process.env.API_URL}/coupons/apply-coupon`
+  const req = await fetch(url, {
+    method: "POST",
+    headers: {
+      'Content-Type' : 'application/json'
+    },
+    body: JSON.stringify(coupon)
+  })
+  const response = await req.json()
+  return Response.json({...response, status: req.status})
 }

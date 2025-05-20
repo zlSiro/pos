a9069ccbd79abb16ed57e7347a5878f5cb8bf396
JuanPablo Valdebenito <jpablo.valdebenitom@gmail.com>
2025-05-20 15:54:04 -0400

    tag para refrescar la pagina despues de confirmar la compra

--- frontend/app/(store)/[categoryId]/page.tsx
+++ frontend/app/(store)/[categoryId]/page.tsx
@@ -6,7 +6,11 @@ type Params = Promise<{categoryId: string}>
 
 async function getProducts(categoryId : string) {
   const url = `${process.env.API_URL}/categories/${categoryId}?products=true`
-  const req = await fetch(url)
+  const req = await fetch(url, {
+    next: {
+      tags: ["products-by-category"]
+    }
+  })
   const json = await req.json()
   if (!req.ok) { //si la consulta no es correcta o no existe lo redireccionamos
     redirect("/1")

83b96139f583441e659c6a8fb341b49e9f98c856
JuanPablo Valdebenito <jpablo.valdebenitom@gmail.com>
2025-05-20 19:38:29 -0400

    + schemas de zod para tipado

--- frontend/src/schemas.ts
+++ frontend/src/schemas.ts
@@ -1,68 +1,90 @@
 import { z } from "zod";
 
 export const ProductSchema = z.object({
-    id: z.number(),
-    name: z.string(),
-    image: z.string(),
-    price: z.coerce.number(),
-    inventory: z.number(),
-    categoryId: z.number()
-})
+  id: z.number(),
+  name: z.string(),
+  image: z.string(),
+  price: z.coerce.number(),
+  inventory: z.number(),
+  categoryId: z.number(),
+});
 
 export const CategorySchema = z.object({
-    id: z.number(),
-    name: z.string()
-})
+  id: z.number(),
+  name: z.string(),
+});
 
-export const CategoriesResponseSchema = z.array(CategorySchema)
+export const CategoriesResponseSchema = z.array(CategorySchema);
 
 export const CategoryWithProductsResponseSchema = CategorySchema.extend({
-    products: z.array(ProductSchema)
+  products: z.array(ProductSchema),
 });
 
 /** Shopping Cart */
 const ShoppingCartContentsSchema = ProductSchema.pick({
-    name: true,
-    image: true,
-    price: true,
-    inventory: true
+  name: true,
+  image: true,
+  price: true,
+  inventory: true,
 }).extend({
-    productId: z.number(),
-    quantity: z.number()
-})
+  productId: z.number(),
+  quantity: z.number(),
+});
 
-export const ShoppingCartSchema = z.array(ShoppingCartContentsSchema)
+export const ShoppingCartSchema = z.array(ShoppingCartContentsSchema);
 
 export const CouponResponseSchema = z.object({
-    name: z.string().default(''),
-    message: z.string(),
-    percentage: z.coerce.number().min(0).max(100).default(0)
-})
+  name: z.string().default(""),
+  message: z.string(),
+  percentage: z.coerce.number().min(0).max(100).default(0),
+});
 
 const OrderContentSchema = z.object({
   productId: z.number(),
   quantity: z.number(),
-  price: z.number()
-})
+  price: z.number(),
+});
 
 export const OrderSchema = z.object({
   total: z.number(),
   coupon: z.string(),
-  contents: z.array(OrderContentSchema).min(1, {message: 'El Carrito no puede ir vacio'})
-})
+  contents: z
+    .array(OrderContentSchema)
+    .min(1, { message: "El Carrito no puede ir vacio" }),
+});
 
 /** Success / Error Response */
 export const SuccessResponseSchema = z.object({
-  message: z.string()
-})
+  message: z.string(),
+});
 
 export const ErrorResponseSchema = z.object({
   message: z.array(z.string()),
   error: z.string(),
-  statusCode: z.number()
-})
+  statusCode: z.number(),
+});
 
-export type Product = z.infer<typeof ProductSchema>
-export type ShoppingCart = z.infer<typeof ShoppingCartSchema>
-export type CartItem = z.infer<typeof ShoppingCartContentsSchema>
-export type Coupon = z.infer<typeof CouponResponseSchema>
+export const ContentsSchema = z.object({
+  id: z.number(),
+  quantity: z.number(),
+  price: z.string(),
+  product: ProductSchema,
+});
+
+export const TransactionResponseSchema = z.object({
+  id: z.number(),
+  total: z.string(),
+  transactionDate: z.string(),
+  discount: z.string(),
+  coupon: z.string().nullable(),
+  contents: z.array(ContentsSchema),
+});
+
+export const TransactionsResponseSchema = z.array(TransactionResponseSchema);
+
+export type Product = z.infer<typeof ProductSchema>;
+export type ShoppingCart = z.infer<typeof ShoppingCartSchema>;
+export type CartItem = z.infer<typeof ShoppingCartContentsSchema>;
+export type Coupon = z.infer<typeof CouponResponseSchema>;
+export type Transaction = z.infer<typeof TransactionResponseSchema>
+

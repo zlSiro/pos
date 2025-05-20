dd99eb87c2f940deb560d06d05ac37bfb4e720cb
JuanPablo Valdebenito <jpablo.valdebenitom@gmail.com>
2025-05-20 14:20:09 -0400

    update return para enviar un objeto con el mensaje

--- backend/src/transactions/transactions.service.ts
+++ backend/src/transactions/transactions.service.ts
@@ -62,7 +62,7 @@ export class TransactionsService {
     })
 
     
-    return "Venta almacenada correctamente";
+    return {message: "Venta almacenada correctamente"};
   }
 
   findAll(transactionDate? : string) {

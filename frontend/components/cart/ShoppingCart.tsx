a362a8ce4f4c53da2c65776f668c5f361a5e226e
JuanPablo Valdebenito <jpablo.valdebenitom@gmail.com>
2025-05-20 13:31:37 -0400

    se agrega el componente CouponForm y se actualiza el componente de Amount para mostrar el descuento en el carrito

--- frontend/components/cart/ShoppingCart.tsx
+++ frontend/components/cart/ShoppingCart.tsx
@@ -3,11 +3,14 @@
 import { useStore } from "@/src/store"
 import ShoppingCartItem from "./ShoppingCartItem"
 import Amount from "./Amount"
+import CouponForm from "./CouponForm"
 
 export default function ShoppingCart() {
 
   const contents = useStore(state => state.contents)
   const total = useStore(state => state.total)
+  const discount = useStore(state => state.discount)
+
   return (
     <>
       {contents.length ? (
@@ -22,11 +25,21 @@ export default function ShoppingCart() {
             ))}
           </ul>
           <dl className="space-y-6 border-t border-gray-300 py-6 text-sm font-medium text-gray-500">
+            { discount ? (
+              <Amount
+                label="Descuento: "
+                amount={discount}
+                discount={true}
+              />
+            ): null }
+            
             <Amount
               label="Total a Pagar: "
               amount={total}
             />
           </dl>
+
+          <CouponForm />
         </>
       ) : (
         <p className="text-xl text-center text-gray-900">El carrito esta vacio</p>

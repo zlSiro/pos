676bec53ccee20919aadc7b1808e2301933bb7ce
JuanPablo Valdebenito <jpablo.valdebenitom@gmail.com>
2025-05-20 19:36:29 -0400

    creada la funcion de la pagina de sales

--- frontend/app/admin/sales/page.tsx
+++ frontend/app/admin/sales/page.tsx
@@ -1,5 +1,29 @@
-export default function SalesPage() {
+import TransactionFilter from "@/components/transactions/transactionFilter";
+import Heading from "@/components/ui/Heading";
+import { getSalesByDate } from "@/src/api";
+import { QueryClient, dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { format } from "date-fns";
+
+export default async function SalesPage() {
+
+  const queryClient = new QueryClient()
+
+  const today = new Date()
+  const formattedDate = format(today, "yyyy-MM-dd")
+  await queryClient.prefetchQuery({
+    queryKey: ["sales", formattedDate],
+    queryFn: () => getSalesByDate(formattedDate)
+  })
+
   return (
-    <div>SalesPage</div>
+    <>
+      <Heading>Ventas</Heading>
+      <p className="text-lg">En esta seccion podras ver las ventas, utiliza el calendario para filtrar por fecha.</p>
+
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <TransactionFilter />
+      </HydrationBoundary>
+
+    </>
   )
 }

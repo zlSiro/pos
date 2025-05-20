5cf89709352b95ebb46530dc75d09a0048a2faf8
JuanPablo Valdebenito <jpablo.valdebenitom@gmail.com>
2025-05-20 13:30:53 -0400

    creacion del componente con la logica para aplicar el descuento

--- frontend/components/cart/CouponForm.tsx
+++ frontend/components/cart/CouponForm.tsx
@@ -1,7 +1,41 @@
-
+import { FormEvent } from "react"
+import { useStore } from "@/src/store"
 
 export default function CouponForm() {
+
+  const applyCoupon = useStore(state => state.applyCoupon)
+  const coupon = useStore(state => state.coupon)
+
+  const handleSubmit = async (event : FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    const formData = new FormData(event.currentTarget)
+    const couponName = formData.get("coupon_name")?.toString()!
+    if(!couponName.length) return
+    await applyCoupon(couponName)
+  }
+
   return (
-    <div>CouponForm</div>
+    <>
+    <p className="py-5 font-bold border-t border-gray-300">Canjear Cupón</p>
+          <form 
+            className="flex" 
+            onSubmit={handleSubmit}
+          >
+            <input 
+                type="text"
+                className="p-2 bg-gray-200 border-gray-300 w-full"
+                placeholder="Ingresa un cupón"
+                name="coupon_name"
+            />
+            <input 
+                type="submit"
+                className="p-3 bg-green-400 font-bold hover:cursor-pointer"
+                value='Canjear'
+            />
+          </form>
+          {coupon.message ? (
+            <p className="py-4 text-center text-sm font-bold">{coupon.message}</p>
+          ) : null}
+    </>
   )
 }

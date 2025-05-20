be642aaba2c36420dc86cf7bcda9222686fb2479
JuanPablo Valdebenito <jpablo.valdebenitom@gmail.com>
2025-05-20 19:37:16 -0400

    + logica al componente

--- frontend/components/transactions/transactionFilter.tsx
+++ frontend/components/transactions/transactionFilter.tsx
@@ -4,6 +4,10 @@ import { useState } from "react"
 import Calendar from "react-calendar"
 import "react-calendar/dist/Calendar.css"
 import { format } from "date-fns"
+import { useQuery } from "@tanstack/react-query"
+import { getSalesByDate } from "@/src/api"
+import TransactionSummary from "./TransactionSummary"
+import { formatCurrency } from "@/src/utils"
 
 type ValuePiece = Date | null
 type Value = ValuePiece | [ValuePiece, ValuePiece]
@@ -12,19 +16,36 @@ export default function TransactionFilter() {
 
   const [ date, setDate ] = useState<Value>(new Date())
   const formattedDate = format(date?.toString()!, "yyyy-MM-dd")
-  console.log(formattedDate)
+
+  const { data, isLoading } = useQuery({
+    queryKey: ["sales", formattedDate],
+    queryFn: () => getSalesByDate(formattedDate)
+  })
+
+  const total = data?.reduce((total, transaction) => total + +transaction.total, 0) ?? 0
 
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 mt-10">
-      <div>
+    <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 mt-10 relative items-start">
+      <div className="lg:sticky lg:top-10">
         <Calendar
           value={date}
           onChange={setDate}
+          locale="es"
         />
       </div>
 
       <div>
-        2
+        {isLoading && "Cargando..."}
+        { data ? data.length ? data.map(transaction => (
+          <TransactionSummary
+            key={transaction.id}
+            transaction={transaction}
+          />
+        )) : <p className="tex-lg text-center">No hay ventas en esta fecha</p> : null }
+
+        <p className="my-5 text-lg font-bold text-right">Total del dia: {''}
+          <span className="font-normal">{formatCurrency(total)}</span>
+        </p>
       </div>
     </div>
   )

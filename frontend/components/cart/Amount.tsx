ef6993c9d29c874d41037d21a32d84fdea4aa8cc
JuanPablo Valdebenito <jpablo.valdebenitom@gmail.com>
2025-05-20 13:30:29 -0400

    actualizacion del componente

--- frontend/components/cart/Amount.tsx
+++ frontend/components/cart/Amount.tsx
@@ -3,16 +3,17 @@ import { formatCurrency } from "@/src/utils"
 type AmountProps = {
   label: string
   amount: number
+  discount?: boolean
 }
 
-export default function Amount({label, amount} : AmountProps) {
+export default function Amount({label, amount, discount} : AmountProps) {
   return (
-    <div className="flex justify-between">
+    <div className={`${discount && "bg-green-400 text-green-900"} flex justify-between p-1`}>
       <dt className="font-bold">
         {label}
       </dt>
       <dd className="text-gray-900">
-        {formatCurrency(amount)}
+        {discount && '-'}{formatCurrency(amount)}
       </dd>
     </div>
   )

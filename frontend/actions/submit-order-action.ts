3a02162b686209f5038586f30301db25fe92f934
JuanPablo Valdebenito <jpablo.valdebenitom@gmail.com>
2025-05-20 15:53:11 -0400

    Server action para el useEfect

--- frontend/actions/submit-order-action.ts
+++ frontend/actions/submit-order-action.ts
@@ -1,5 +1,31 @@
 "use server"
 
-export async function submitOrder() {
-  console.log("Desde submitOrder")
+import { ErrorResponseSchema, OrderSchema, SuccessResponseSchema } from "@/src/schemas"
+import { revalidateTag } from "next/cache"
+
+export async function submitOrder(data: unknown) {
+  const order = OrderSchema.parse(data)
+  const url = `${process.env.API_URL}/transactions`
+  const req = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({...order})
+  })
+  const json = await req.json()
+  if(!req.ok) {
+    const errors = ErrorResponseSchema.parse(json)
+    return {
+      errors: errors.message.map(issue => issue),
+      success: ""
+    }
+  }
+  const success = SuccessResponseSchema.parse(json)
+  revalidateTag("products-by-category")
+
+  return {
+    errors: [],
+    success: success.message
+  }
 }

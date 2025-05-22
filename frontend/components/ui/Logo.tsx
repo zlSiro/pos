d60e2501b90af032961dca1067337b874a659908
JuanPablo Valdebenito <jpablo.valdebenitom@gmail.com>
2025-05-22 08:59:15 -0400

    Update

--- frontend/components/ui/Logo.tsx
+++ frontend/components/ui/Logo.tsx
@@ -1,7 +1,7 @@
 export default function Logo() {
   return (
     <h1 className="text-3xl font-extrabold text-white">
-      POS {""}
+      Ferreteria FERRAMAS {""}
       <span className="text-green-400 text-xl ">Next.js / Nest</span>
     </h1>
   );

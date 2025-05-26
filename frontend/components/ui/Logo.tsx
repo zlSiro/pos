c418e5cfecf48e42d3b66e132a8eabe221a02628
JuanPablo Valdebenito <jpablo.valdebenitom@gmail.com>
2025-05-26 09:19:17 -0400

    Actualizado logo

--- frontend/components/ui/Logo.tsx
+++ frontend/components/ui/Logo.tsx
@@ -2,7 +2,7 @@ export default function Logo() {
   return (
     <h1 className="text-3xl font-extrabold text-white">
       Ferreteria FERRAMAS {""}
-      <span className="text-green-400 text-xl ">Next.js / Nest</span>
+      <span className="text-green-400 text-xl ">{'    '} Su ferreteria de confianza!</span>
     </h1>
   );
 }

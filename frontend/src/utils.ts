eca31db01a69f8b229183eecbcff277697ebe65b
JuanPablo Valdebenito <jpablo.valdebenitom@gmail.com>
2025-05-26 09:18:13 -0400

    Funcion para comprobar si la paginacion es valida

--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -1,3 +1,23 @@
 export function formatCurrency(amount: number) {
   return new Intl.NumberFormat('en-US', { style: "currency", currency: "USD"}).format(amount)
 }
+
+
+export function isValidPage(value: number) {
+    if (value == null) {
+      return false;
+    }
+    
+    if (typeof value !== 'number' && isNaN(value)) {
+      return false;
+    }
+    if (value <= 0) {
+      return false;
+    }
+  
+    if (!Number.isInteger(value)) {
+      return false;
+    }
+
+    return true;
+}

b8ebec868c1736eb7ff8c2b2d4519a3bfa463f0b
JuanPablo Valdebenito <jpablo.valdebenitom@gmail.com>
2025-05-26 09:27:02 -0400

    creada la vista para añadir nuevos productos

--- frontend/app/admin/products/page.tsx
+++ frontend/app/admin/products/page.tsx
@@ -1,5 +1,55 @@
-export default function ProductsPage() {
+import ProductsTable from "@/components/products/ProductsTable";
+import Heading from "@/components/ui/Heading";
+import Pagination from "@/components/ui/Pagination";
+import { ProductResponseSchema } from "@/src/schemas";
+import { isValidPage } from "@/src/utils";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+
+async function getProducts(take : number, skip : number) {
+  const url = `${process.env.API_URL}/products?take=${take}&skip=${skip}`
+  const req = await fetch(url)
+  const json = await req.json()
+  const data = ProductResponseSchema.parse(json)
+  return {
+    products: data.products,
+    total: data.total
+  }
+}
+
+type SearchParams = Promise<{page: string}>
+
+export default async function ProductsPage({searchParams} : {searchParams: SearchParams}) {
+
+  const { page } = await searchParams
+
+  if(!isValidPage(+page)) redirect("/admin/products?page=1")
+
+  const productsPerPage = 10
+  const skip = (+page - 1) *  productsPerPage
+  const {products, total} = await getProducts(productsPerPage, skip)
+
+  const totalPages = Math.ceil(total / productsPerPage)
+  if (+page > totalPages) redirect("/admin/products?page=1")
+
   return (
-    <div>ProductsPage</div>
+    <>
+      <Link
+        href="/admin/products/new"
+        className="rounded bg-green-400 font-bold py-2 px-10"
+      >Nuevo Producto</Link>
+
+      <Heading>Administrar Productos</Heading>
+
+      <ProductsTable
+        products={products}
+      />
+
+      <Pagination
+        page={+page}
+        totalPages={totalPages}
+        baseUrl="/admin/products"
+      />
+    </>
   )
 }

3b7579127adbb20752e968b309c4945cdd073fb5
JuanPablo Valdebenito <jpablo.valdebenitom@gmail.com>
2025-05-26 09:28:06 -0400

    creada la vista para añadir nuevos productos

--- frontend/app/admin/products/new/page.tsx
+++ frontend/app/admin/products/new/page.tsx
@@ -1,7 +1,22 @@
+import AddProductForm from "@/components/products/AddProductForm";
+import ProductForm from "@/components/products/ProductForm";
+import Heading from "@/components/ui/Heading";
+import Link from "next/link";
 
 
 export default function NewProductPage() {
   return (
-    <div>NewProductPage</div>
+    <>
+      <Link
+        href="/admin/products?page=1"
+        className="rounded bg-green-400 font-bold py-2 px-10"
+      >Volver</Link>
+      
+      <Heading>Nuevo Producto</Heading>
+
+      <AddProductForm>
+        <ProductForm />
+      </AddProductForm>
+    </>
   )
 }

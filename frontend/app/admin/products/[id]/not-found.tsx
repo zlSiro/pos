6b5d3ca5ca59d52dfff42122c51aef428cb58dbd
JuanPablo Valdebenito <jpablo.valdebenitom@gmail.com>
2025-05-26 09:27:18 -0400

    creada la pagina 404 not found

--- frontend/app/admin/products/[id]/not-found.tsx
+++ frontend/app/admin/products/[id]/not-found.tsx
@@ -1,7 +1,17 @@
+import Heading from "@/components/ui/Heading";
+import Link from "next/link";
 
 
 export default function NotFound() {
   return (
-    <div>NotFound</div>
+    <div className="text-center">
+      <Heading>Producto No Encontrado</Heading>
+
+      <p>Tal vez quieras volver a {" "}
+        <Link className="text-green-400"
+        href={"/admin/products?page=1"}
+        >Productos</Link>
+      </p>
+    </div>
   )
 }

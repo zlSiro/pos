e47428f9f20504b6466211b0c783649946727aa1
JuanPablo Valdebenito <jpablo.valdebenitom@gmail.com>
2025-05-26 09:23:04 -0400

    funcion para eliminar un producto

--- frontend/components/products/DeleteProductForm.tsx
+++ frontend/components/products/DeleteProductForm.tsx
@@ -1,6 +1,28 @@
-export default function DeleteProductForm() {
+import { Product } from "@/src/schemas";
+import { revalidatePath } from "next/cache";
+
+export default function DeleteProductForm({productId} : {productId : Product["id"]}) {
+
+  const handleDeleteProduct = async () => {
+    "use server"
+
+    const url = `${process.env.API_URL}/products/${productId}`
+    const req = await fetch(url, {
+      method: "DELETE"
+    })
+    await req.json()
+    revalidatePath("/admin/products")
+  }
   return (
     
-    <p></p>
+    <form
+      action={handleDeleteProduct}
+    >
+      <input
+        type="submit"
+        className="text-red-600 hover:text-red-800 cursor-pointer"
+        value="Eliminar"
+      />
+    </form>
   );
 }

a8c8ade111349de45e77673b2d327d473f0d91fa
JuanPablo Valdebenito <jpablo.valdebenitom@gmail.com>
2025-05-26 09:27:44 -0400

    creada la vista para la edicion de productos

--- frontend/app/admin/products/[id]/edit/page.tsx
+++ frontend/app/admin/products/[id]/edit/page.tsx
@@ -1,8 +1,29 @@
+import EditProductForm from "@/components/products/EditProductForm";
+import ProductForm from "@/components/products/ProductForm";
 import Heading from "@/components/ui/Heading";
+import { ProductSchema } from "@/src/schemas";
 import Link from "next/link";
+import { notFound } from "next/navigation";
 
+async function getProduct(id: string) {
+  const url = `${process.env.API_URL}/products/${id}`
+  const req = await fetch(url)
+  const json = await req.json()
 
-export default function EditProductPage() {
+  if (!req.ok) {
+    notFound()
+  }
+  const product = ProductSchema.parse(json)
+  return product
+}
+
+type Params = Promise<{id: string}>
+
+export default async function EditProductPage({params} : {params : Params}) {
+
+  const { id } = await params
+  const product = await getProduct(id)
+  
   return (
     <>
       <Link
@@ -12,7 +33,11 @@ export default function EditProductPage() {
 
       <Heading>Editar Producto: {}</Heading>
 
-      
+      <EditProductForm>
+        <ProductForm
+          product={product}
+        />
+      </EditProductForm>
     </>
   )
 }

3d0ca6686870b93ff06bdacf65ade2980b91227e
JuanPablo Valdebenito <jpablo.valdebenitom@gmail.com>
2025-05-26 09:22:18 -0400

    creada la funcionalidad para el cation de actualizar el producto

---
+++ frontend/actions/update-product-action.ts
@@ -0,0 +1,51 @@
+"use server"
+
+import { ErrorResponseSchema, Product, ProductFormSchema } from "@/src/schemas"
+
+type ActionStateType = {
+  errors:string[]
+  success: string
+}
+
+export async function updateProduct( productId: Product["id"], prevState: ActionStateType, formData: FormData ) {
+  
+  const product = ProductFormSchema.safeParse({
+    name: formData.get("name"),
+    price: formData.get("price"),
+    inventory: formData.get("inventory"),
+    categoryId: formData.get("categoryId"),
+  })
+
+  if(!product.success) {
+    return {
+      errors: product.error.issues.map(issue => issue.message),
+      success: ""
+    }
+  }
+
+  // Si no hay error, Comunicarse con la api
+  const url = `${process.env.API_URL}/products/${productId}`
+  const req = await fetch(url, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(product.data)
+  })
+  const json = await req.json()
+
+  if (!req.ok) {
+    const errors = ErrorResponseSchema.parse(json)
+    return {
+      errors: errors.message.map(issue => issue),
+      success: ""
+    }
+  }
+
+
+
+  return {
+    errors: [],
+    success: "Producto Actualizado Correctamente"
+  }
+}

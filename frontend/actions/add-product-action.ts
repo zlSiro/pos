b186116b0213a64f854d55e378d1d2bafe5a5013
JuanPablo Valdebenito <jpablo.valdebenitom@gmail.com>
2025-05-26 09:25:30 -0400

    + action para agregar productos en la BD

--- frontend/actions/add-product-action.ts
+++ frontend/actions/add-product-action.ts
@@ -1,15 +1,51 @@
 "use server"
 
+import { ErrorResponseSchema, ProductFormSchema } from "@/src/schemas"
+
 type ActionStateType = {
   errors:string[]
   success: string
 }
 
 export async function addProduct(prevState: ActionStateType, formData: FormData) {
-  console.log("desde addProduct")
+  
+  const product = ProductFormSchema.safeParse({
+    name: formData.get("name"),
+    price: formData.get("price"),
+    inventory: formData.get("inventory"),
+    categoryId: formData.get("categoryId"),
+  })
+
+  if(!product.success) {
+    return {
+      errors: product.error.issues.map(issue => issue.message),
+      success: ""
+    }
+  }
+
+  // Si no hay error, Comunicarse con la api
+  const url = `${process.env.API_URL}/products`
+  const req = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(product.data)
+  })
+  const json = await req.json()
+
+  if (!req.ok) {
+    const errors = ErrorResponseSchema.parse(json)
+    return {
+      errors: errors.message.map(issue => issue),
+      success: ""
+    }
+  }
+
+
 
   return {
     errors: [],
-    success: ""
+    success: "Producto Agregado Correctamente"
   }
 }

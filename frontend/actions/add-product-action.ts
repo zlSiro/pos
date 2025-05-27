b6ac1539f190c709a7b0d0ad7e68a3c73b129617
JuanPablo Valdebenito <jpablo.valdebenitom@gmail.com>
2025-05-27 14:49:07 -0400

    + image al schema de zod de addProduct action

--- frontend/actions/add-product-action.ts
+++ frontend/actions/add-product-action.ts
@@ -12,6 +12,7 @@ export async function addProduct(prevState: ActionStateType, formData: FormData)
   const product = ProductFormSchema.safeParse({
     name: formData.get("name"),
     price: formData.get("price"),
+    image: formData.get("image"),
     inventory: formData.get("inventory"),
     categoryId: formData.get("categoryId"),
   })

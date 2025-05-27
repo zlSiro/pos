a9d44db477abc91b38f85bcaf68ef659bff8dc07
JuanPablo Valdebenito <jpablo.valdebenitom@gmail.com>
2025-05-27 14:51:37 -0400

    update component

--- frontend/components/products/productCard.tsx
+++ frontend/components/products/productCard.tsx
@@ -1,5 +1,5 @@
 import { Product } from "@/src/schemas";
-import { formatCurrency } from "@/src/utils";
+import { formatCurrency, getImagePath, isAvailable } from "@/src/utils";
 import Image from "next/image";
 import AddProductButton from "./AddProductButton";
 
@@ -8,9 +8,9 @@ export default function ProductCard({product} : {product : Product}) {
     <div
       className='rounded bg-white shadow relative p-5'
     >
-      <div>
+      <div className={`${!isAvailable(product.inventory) && 'opacity-40'}`}>
         <Image 
-          src={`${process.env.API_URL}/img/${product.image}`}
+          src={getImagePath(product.image)}
           alt={`Imagen de Producto ${product.name}`}
           width={400}
           height={600}
@@ -21,10 +21,19 @@ export default function ProductCard({product} : {product : Product}) {
             <p className="text-gray-500">Disponibles: {product.inventory}</p>
             <p className="text-2xl font-extrabold  text-gray-900">{formatCurrency(product.price)}</p>
         </div>
-      </div> 
-      <AddProductButton
-        product={product}
-      />     
+      </div>
+      {
+        isAvailable(product.inventory) ? (
+          <AddProductButton
+            product={product}
+          />
+        ) : (
+          <p 
+            className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white opacity-60
+            w-full text-center py-5 text-2xl uppercase font-black"
+          >Agotado</p>
+        )}
+          
     </div>
   )
 }

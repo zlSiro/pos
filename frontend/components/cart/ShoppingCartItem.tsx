ee49d34ef8d70b466921461d12c9389c2c976f86
JuanPablo Valdebenito <jpablo.valdebenitom@gmail.com>
2025-05-27 14:50:43 -0400

    update de la funcion para obtener el path de la imagen

--- frontend/components/cart/ShoppingCartItem.tsx
+++ frontend/components/cart/ShoppingCartItem.tsx
@@ -1,5 +1,5 @@
 import { CartItem } from "@/src/schemas";
-import { formatCurrency } from "@/src/utils";
+import { formatCurrency, getImagePath } from "@/src/utils";
 import Image from "next/image";
 import { useStore } from "@/src/store";
 
@@ -12,7 +12,7 @@ export default function ShoppingCartItem({item} : {item : CartItem}) {
     <li className="flex items-center space-x-6 py-6 relative">
       <div className="h-24 w-24">
         <Image
-          src={`${process.env.NEXT_PUBLIC_API_URL}/img/${item.image}`}
+          src={getImagePath(item.image)}
           alt={`Imagen del producto ${item.name}`}
           width={100}
           height={100}

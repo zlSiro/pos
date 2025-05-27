c756b113ef4574f4489182bffaff5edc5c979838
JuanPablo Valdebenito <jpablo.valdebenitom@gmail.com>
2025-05-27 14:36:20 -0400

    + fruncion para buscar el path de la imagen

--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -21,3 +21,20 @@ export function isValidPage(value: number) {
 
     return true;
 }
+
+export function getImagePath( image : string ) {
+
+  const cloudinaryBaseUrl = "https://res.cloudinary.com"
+
+  if (image.startsWith(cloudinaryBaseUrl)) {
+    return image
+  } else {
+    if (process.env.API_URL) {
+      return `${process.env.API_URL}/img/${image}`
+    } else {
+      return `${process.env.NEXT_PUBLIC_API_URL}/img/${image}`
+    }
+  }
+}
+
+export const isAvailable = ( inventory : number ) => inventory > 0

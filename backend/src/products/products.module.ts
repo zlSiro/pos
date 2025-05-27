202c8d392db0f654b8135d8e5c651bf8a87fa19e
JuanPablo Valdebenito <jpablo.valdebenitom@gmail.com>
2025-05-27 14:38:43 -0400

    import de modulo UploadImage

--- backend/src/products/products.module.ts
+++ backend/src/products/products.module.ts
@@ -4,11 +4,10 @@ import { ProductsController } from './products.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from './entities/product.entity';
 import { Category } from '../categories/entities/category.entity';
+import { UploadImageModule } from 'src/upload-image/upload-image.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([Product, Category])
-  ],
+  imports: [TypeOrmModule.forFeature([Product, Category]), UploadImageModule],
   controllers: [ProductsController],
   providers: [ProductsService],
 })

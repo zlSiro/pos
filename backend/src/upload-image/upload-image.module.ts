60322cf77a7905b65f43171d02a048191672378e
JuanPablo Valdebenito <jpablo.valdebenitom@gmail.com>
2025-05-27 14:38:00 -0400

    import de provider de uploadImage

--- backend/src/upload-image/upload-image.module.ts
+++ backend/src/upload-image/upload-image.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UploadImageService } from './upload-image.service';
-import { UploadImage } from './upload-image';
+import { UploadImageProvider } from './upload-image';
 
 @Module({
-  providers: [UploadImageService, UploadImage]
+  providers: [UploadImageService, UploadImageProvider],
+  exports: [UploadImageService, UploadImageProvider],
 })
 export class UploadImageModule {}

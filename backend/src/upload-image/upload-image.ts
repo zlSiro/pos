9b7bfb337b4df6d9c14bfba1c4784ac5c0c077c5
JuanPablo Valdebenito <jpablo.valdebenitom@gmail.com>
2025-05-27 14:48:29 -0400

    provider para la conexion con cloudinary

--- backend/src/upload-image/upload-image.ts
+++ backend/src/upload-image/upload-image.ts
@@ -1,4 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { v2 as cloudinary } from "cloudinary"
 
-@Injectable()
-export class UploadImage {}
+export const UploadImageProvider = {
+
+  provide: 'CLOUDINARY',
+  useFactory: () => {
+    return cloudinary.config({
+      cloud_name: process.env.CLOUDINARY_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET,
+    })
+  }
+}

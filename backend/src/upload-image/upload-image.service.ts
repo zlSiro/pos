bc680c6ae2a3fe27772e173aa310233ae8aa54b8
JuanPablo Valdebenito <jpablo.valdebenitom@gmail.com>
2025-05-27 14:38:22 -0400

    creada ka funcion para subir imagenes a cloudinary

--- backend/src/upload-image/upload-image.service.ts
+++ backend/src/upload-image/upload-image.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { v2 as cloudinary } from 'cloudinary';
+import { CloudinaryResponse } from './upload-image.response';
+//import streamifier from 'streamifier';
+const streamifier = require('streamifier')
 
 @Injectable()
-export class UploadImageService {}
+export class UploadImageService {
+
+  uploadFile(file : Express.Multer.File) : Promise<CloudinaryResponse> {
+    return new Promise<CloudinaryResponse>((resolve, reject) => {
+      const uploadStream = cloudinary.uploader.upload_stream(
+        (error, result) => {
+          if(error) return reject(new Error(`Error al subir archivo a Cloudinary: ${error.message}`));
+          if(!result) return reject (new Error("No hay resultados desde Cloudinary"));
+          resolve(result)
+        }
+      )
+      streamifier.createReadStream(file.buffer).pipe(uploadStream)
+    })
+  }
+}

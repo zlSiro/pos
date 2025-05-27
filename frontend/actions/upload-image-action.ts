f696c8aa02be8f1ccf1dbc50f33a20d4aa8d79a3
JuanPablo Valdebenito <jpablo.valdebenitom@gmail.com>
2025-05-27 14:50:09 -0400

    Creada la funcion para el actionde subir fotos a cloudinary

--- frontend/actions/upload-image-action.ts
+++ frontend/actions/upload-image-action.ts
@@ -1,1 +1,11 @@
 "use server"
+
+export async function uploadImage(formData: FormData) : Promise<string> {
+  const url = `${process.env.API_URL}/products/upload-image`
+  const req = await fetch(url, {
+    method: "POST",
+    body: formData
+  })
+  const image = await req.json()
+  return image.secure_url
+}
